d6d1e301c6a43887297434335aaaa5de2ab8293b
Alejandro Salas <asalas.wd@outlook.com>
2022-11-11 23:14:44 -0500

    Remove JFrame from ConfirmBox constructor

--- src/com/java21days/ConfirmBox.java
+++ src/com/java21days/ConfirmBox.java
@@ -1,18 +1,15 @@
 package com.java21days;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class ConfirmBox extends JFrame {
-    Dimension dim = new Dimension(300, 300);
 
     ConfirmBox(String title){
-        super(title);
-        setSize(dim);
-        int response = JOptionPane.showConfirmDialog(this, "Do you want to continue?");
-
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
+        int response = JOptionPane.showConfirmDialog(
+                null,
+                "Do you want to continue?", title ,
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+        );
     }
 }
