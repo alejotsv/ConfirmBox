cedc2cd9c6b5e21a06a3f148d30f16bd5f6552f4
Alejandro Salas <asalas.wd@outlook.com>
2022-11-12 15:46:38 -0500

    Add getter for response

--- src/com/java21days/OptionBox.java
+++ src/com/java21days/OptionBox.java
@@ -3,8 +3,22 @@ package com.java21days;
 import javax.swing.*;
 
 public class OptionBox extends JFrame {
-    OptionBox(String title, String message, String[] options){
-        super(title);
+    int response;
 
+    OptionBox(String title, String message, String[] options){
+        response = JOptionPane.showOptionDialog(
+                null,
+                message,
+                title,
+                0,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                options,
+                options[0]
+                );
+    }
+
+    int getResponse(){
+        return response;
     }
 }
